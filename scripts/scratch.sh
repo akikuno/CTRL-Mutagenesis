da2d86ee7ad5de5d08d234771fffbc2da07ee328
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-01 16:13:29 +0900

    count read numbers

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -15,3 +15,26 @@ for fq in data/Alignment_1/20220227_214619/Fastq/*.gz; do
     echo $fq
     fastqc "$fq" -o reports/fastqc
 done
+
+###############################################################################
+# count read numbers
+###############################################################################
+
+: >tmp.csv
+
+for fq in data/Alignment_1/20220227_214619/Fastq/*R1*.gz; do
+    sample_name=$(basename "$fq" | cut -d "_" -f 1)
+    # 辞書順にソートする都合上、サンプル番号1-1などを"1-01”と変更いたします
+    sample_name=$(echo $sample_name | sed "s/-\([1-9]\)$/-0\1/")
+    echo $sample_name
+    # リード数を数えます
+    gzip -dc "$fq" |
+        grep "^@" |
+        wc -l |
+        sed "s/^/${sample_name},/" |
+        cat >>tmp.csv
+done
+
+sort tmp.csv >reports/read_numbers.csv
+
+rm tmp.csv
