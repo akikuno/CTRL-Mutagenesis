1ddd723f018d8a3ff69bb61ce406df1a1eeade35
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-01 17:40:07 +0900

    upload read numbers by gRNA

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -38,3 +38,33 @@ done
 sort tmp.csv >reports/read_numbers.csv
 
 rm tmp.csv
+
+###############################################################################
+# count gRNAs
+###############################################################################
+
+: >tmp.csv
+for fq in data/Alignment_1/20220227_214619/Fastq/*.gz; do
+    sample_name=$(basename "$fq" | cut -d "_" -f 1)
+    if echo "$fq" | grep -q "_R1_"; then
+        sample_name="$sample_name"_R1
+    else
+        sample_name="$sample_name"_R2
+    fi
+    sample_name=$(echo $sample_name | sed "s/-\([1-9]\)_/-0\1_/")
+    # 17種類のgRNAに対してマッチしたリードをカウントします
+    for i in $(seq 1 17); do
+        id=$(cat misc/grna.csv | cut -d, -f 1 | head -n "$i" | tail -n 1)
+        grna_fw=$(cat misc/grna.csv | cut -d, -f 2 | head -n "$i" | tail -n 1)
+        grna_rv=$(echo "$grna_fw" | tr "ACGT" "TGCA" | rev)
+        gzip -dc "$fq" |
+            grep -e "$grna_fw" -e "$grna_rv" |
+            wc -l |
+            sed "s|^|${sample_name},${id},${grna_fw},${grna_rv},|" |
+            cat >>tmp.csv
+    done
+done
+
+sort tmp.csv >reports/read_numbers_by_grnas.csv
+
+rm tmp.csv
